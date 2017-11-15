8703bc23895655437f1800d8fc109e9254787d9f
Lawrence Oks <la161961@ucf.edu>
2017-11-15 16:51:29 -0500

    added initial files

---
+++ js/site.js
@@ -0,0 +1,60 @@
+//Initialize skrollr
+if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+        forceHeight: false
+    });
+}
+
+$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
+
+
+
+var populateProject = function(card) {
+	var name = card.data("name");
+	var date = card.data("date");
+	var tag = card.data("tag");
+	var desc = card.data("desc");
+	// var skills = card.data("skills");
+	var skills = card.data("skills").split(",");
+	var purpose = card.data("purpose");
+	var image = card.data("image");
+
+	$("#projectName").html(name);
+	$("#projectDesc").html(desc);
+	$("#projectDates").html(date);
+	$("#projectPurpose").html(purpose);
+	$("#projectTag").html(tag);
+	$("#projectImage").attr("src", "C:\\Users\\la161961\\lawrence-portfolio\\portfolio 2.0\\img\\" + image + ".jpg");
+
+	if(tag == "") {
+		$("#projectTag").hide();
+	}
+
+	$("#projectSkills").html("");
+
+	$.each(skills, function(index, value) {
+	  $("#projectSkills").append("<span>" + value + "</span>");
+	});
+}
+
+populateProject($("#poltergeistProject"));
+
+$( ".card" ).click(function() {
+	var card = $(this);
+
+	populateProject(card);
+
+});
