10246e69f53563d12b5485a4c719d7b2ba5c66eb
Lawrence Oks <lawrence.oks@ucf.edu>
2019-10-29 17:48:01 -0400

    fixes css and adds nextera picture

--- js/site.js
+++ js/site.js
@@ -51,7 +51,7 @@ var populateProject = function(card) {
 	});
 }
 
-populateProject($("#poltergeistProject"));
+populateProject($("#plannerProject"));
 
 $( ".card" ).click(function() {
 	var card = $(this);
